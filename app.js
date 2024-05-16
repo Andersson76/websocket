809fed7836848337a9c31793f016e570f7287893
Martin Andersson <martin.andersson@me.com>
2024-05-16 10:51:36 +0200

    Added throwDice function

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use(express.static("public"));
 io.on("connection", (socket) => {
   console.log(`A client with id ${socket.id} connected to the chat!`);
 
+  socket.on("rollDice", () => {
+    const diceResult = Math.floor(Math.random() * 6) + 1; // Simulate a dice roll (1-6)
+    io.emit("diceRolled", diceResult);
+  });
+
   socket.on("chatMessage", (msg) => {
     io.emit("newChatMessage", {
       user: msg.user,
